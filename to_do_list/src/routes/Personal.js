43abf6aed8e184fbdb307ff1a4abeef5e47bb61f
kwaksj329 <kwaksj329@gmail.com>
2022-05-28 21:00:13 +0900

    Add: header

--- to_do_list/src/routes/Personal.js
+++ to_do_list/src/routes/Personal.js
@@ -1,37 +1,13 @@
-import { useState } from "react";
+import Header from "../component/Header";
+import Contents from "../component/Contents";
 
-function Home() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo("");
-  };
+function Personal() {
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="Personal..."
-        />
-        <button>Add to DO</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <Header />
+      <Contents />
     </div>
   );
 }
 
-export default Home;
+export default Personal;
